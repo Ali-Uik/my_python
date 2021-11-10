a706547ab665cde19630c089d9a53f0eb22d4966
Ali_Uik <toxic_m@mail.ru>
2021-11-10 23:44:35 +0500

    lesson4 start

---
+++ lesson4/main.py
@@ -0,0 +1,21 @@
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in list:
+#     print('Hello')  # печатает 10 раз  Hello
+
+# range(start=0, finish, step=1)
+#
+# for i in range(10):  # Выведет до -1
+#     print(i)
+#
+# for i in range(3, 10):
+#     print(i)
+
+# list1 = []
+# for i in range(100, 0, -2):
+#     list1.append(i)
+# print(list1)
+
+list2 = []
+for i in range(10):
+    list2.append(i ** 2)
+print(list2)
