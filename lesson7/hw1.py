d2f553cb45b7b082a9b57ee89b51e4944b7067ce
Ali_Uik <toxic_m@mail.ru>
2021-11-22 20:21:04 +0500

    hw1 lesson7

---
+++ lesson7/hw1.py
@@ -0,0 +1,16 @@
+a = input('Введите:')
+
+
+def palindrom(a):
+    list1 = [i for i in a]
+    list2 = list1.reverse()
+    return list1, list2
+
+
+
+    # print(c)
+    # print(e)
+
+
+d = palindrom(a)
+print(d)
