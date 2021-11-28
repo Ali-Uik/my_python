654995b3965a056c3a2866c4faedc57c62ab4aa7
Ali_Uik <toxic_m@mail.ru>
2021-11-28 23:38:49 +0500

    ending lesson10

---
+++ lesson10/ex3.py
@@ -0,0 +1,13 @@
+file = open('mbox-short.txt', mode='r', encoding='utf-8')
+count = {}
+for line in file:
+    if line.startswith('From '):
+        # data = line.split(' ')
+        # time = data[6].split(':')
+        # hours = time[0]
+        hours = line.split(' ')[6].split(':')[0]
+        count[hours] = count.get(hours, 0) + 1
+
+file.close()
+for key, value in sorted(count.items()):
+    print(f'В {key} часов прошло {value} писем')
--- lesson10/main.py
+++ lesson10/main.py
@@ -24,10 +24,43 @@
 # content = 'Hello world\n'   # \n - Переход на новоу строку
 # file.write(content)
 # file.close()
-file = open('text.txt', mode='a', encoding='utf-8')  # добовляеть строку
-content = 'Hello world\n'  # \n - Переход на новоу строку
-file.write(content)
-file.close()
+# file = open('text.txt', mode='a', encoding='utf-8')  # добовляеть строку
+# content = 'Hello world\n'  # \n - Переход на новоу строку
+# file.write(content)
+# file.close()
 
+# Конструкция которая автоматически закрывает файл with open
 
+# with open('pushkin.txt', mode='r', encoding='utf-8') as file:
+#     new_file = open('new_file.txt', mode='w', encoding='utf-8')
+#     content = file.read()
+#     print('Закрыт ли файл?', file.closed)
+#     new_file.write(content)
+#     new_file.close()
+# print('Закрыт ли файл?', file.closed)
 
+# Работа с данными в не текстовых файлах
+# import os
+# if 'picture' not in os.listdir():  # Создаем папку
+#     os.mkdir('picture')
+# file = open('nature.jpg', mode='rb')
+# picture = file.read()
+# for i in range(1, 11):
+#     new_picture = open(f'picture/nature_copy{i}.jpg', mode='wb')
+#     new_picture.write(picture)
+#     new_picture.close()
+# file.close()
+# for i in range(1,11):
+#     os.remove(f'picture/nature_copy{i}.jpg')
+# os.rmdir('picture')
+str1 = 'Hello'
+str1_8 = str1.encode(encoding='utf-8')
+str1_16 = str1.encode(encoding='utf-16')
+str1_32 = str1.encode(encoding='utf-32')
+str1_cp866 = str1.encode(encoding='cp866')
+str1_cp1251 = str1.encode(encoding='cp1251')
+print(str1_8)
+print(str1_16)
+print(str1_32)
+print(str1_cp866)
+print(str1_cp1251)
