4f6f8a9aef79b624150cb611481c4245f45c51dc
Ali_Uik <toxic_m@mail.ru>
2021-11-29 23:23:23 +0500

    any changes

--- lesson10/hw2.py
+++ lesson10/hw2.py
@@ -2,7 +2,15 @@
 # составьте список слов. В списке слова не должны дублироваться. После чего распечатайте список, в котором все слова
 # будут отсортированы в алфавитном порядке.
 from itertools import groupby
+
 file = open('romeo.txt', mode='r', encoding='utf-8')
+data = []
 for line in file:
-    data = line.split('\n')
-    print(data)
+    for i in line:
+        data.append(i)
+data_str = ''
+for i in data:
+    if i != '\n':
+        data_str = data_str + str(i)
+data3 = data_str.split(' ')
+print(data_str)
