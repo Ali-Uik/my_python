e337a02144653748156b48b94dc3a3a6cdfb0d12
Ali_Uik <toxic_m@mail.ru>
2021-12-03 23:08:18 +0500

    lesson 10 dop exersice Nodir's version

--- lesson10/dop5.py
+++ lesson10/dop5.py
@@ -1,12 +1,30 @@
 # Требуется реализовать функцию longest_words(file), которая выводит слово, имеющее максимальную длину (или список слов,
 # если таковых несколько).
-def read_last(file):
-    file2 = open(file, mode='r', encoding='utf-8')
-    data = file2.read()
-    data2 = data.split()
-    max_str = max(data2, key=len)
-    print(max_str)
-    file2.close()
+# def read_last(file):
+#     file2 = open(file, mode='r', encoding='utf-8')
+#     data = file2.read()
+#     data2 = data.split()
+#     max_str = max(data2, key=len)
+#     print(max_str)
+#     file2.close()
+#
+#
+# read_last('articles.txt')
+
+# Nodir's version
+file = open('article.txt', mode='r', encoding='utf-8')
+a = file.read()
+b = a.split()
+print(b)
 
 
-read_last('articles.txt')
+def longest(words):
+    c = ''
+    for i in b:
+        if len(i) > len(c):
+            c = i
+    print('longest one is ', c)
+
+
+longest(b)
+file.close()
