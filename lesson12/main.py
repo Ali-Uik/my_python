308e9daa55b9f2c884fbd0c085d491afe23ad538
Ali_Uik <toxic_m@mail.ru>
2021-12-05 01:05:14 +0500

    lesson12

--- lesson12/main.py
+++ lesson12/main.py
@@ -1,2 +1,16 @@
 # json, virtual environment библиотеки
+from pprint import pprint
 
+json_data = []
+while True:
+    name = input('Введите имя: ')
+    if name.lower() == 'print':
+        pprint(json_data)
+        break
+    lastname = input('Введите фамилию: ')
+    age = input('Введите возраст: ')
+    json_data.append({
+        'Имя': name,
+        'Фамилия': lastname,
+        'Возраст': age
+    })
