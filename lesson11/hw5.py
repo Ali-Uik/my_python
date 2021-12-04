640c33ced162554c381ace7ebea6db0d573b9761
Ali_Uik <toxic_m@mail.ru>
2021-12-04 21:44:53 +0500

    1

--- lesson11/hw5.py
+++ lesson11/hw5.py
@@ -4,19 +4,50 @@ import re
 
 def validateCMD(line):
     # re.search(найди что, найди где)
-    r1 = re.search(r'^CMD[0-9]+', line)
+    r1 = re.search(r'CMD[0-9]+', line)
+    # return r1
+    return r1
+
+
+def validateUN(line):
+    # re.search(найди что, найди где)
+    r1 = re.search(r'UN[0-9]+', line)
+    # return r1
+    return r1
+
+
+def validatePAA(line):
+    # re.search(найди что, найди где)
+    r1 = re.search(r'PAA[0-9]+', line)
+    # return r1
+    return r1
+
+def validateTASH(line):
+    # re.search(найди что, найди где)
+    r1 = re.search(r'01[0-9]+[A-Z]+', line)
+    # return r1
     return r1
 
 
 with open('nomera.txt', mode='r', encoding='utf-8') as file:
-    cmd = open('cdm.txt', mode='w', encoding='utf-8')
+    cmd = open('cmd.txt', mode='w', encoding='utf-8')
+    un = open('un.txt', mode='w', encoding='utf-8')
+    paa = open('paa.txt', mode='w', encoding='utf-8')
+    tash = open('tash.txt', mode='w', encoding='utf-8')
     data = file.read().split(',')
     for line in data:
         # print(line)
-        print(validateCMD(line))
+        if validateCMD(line):
+            cmd.write(line)
+        elif validateUN(line):
+            un.write(line)
+        elif validatePAA(line):
+            paa.write(line)
+        elif validateTASH(line):
+            # tash.write(line)
+            print(line)
 
     cmd.close()
-
-
-
-
+    un.close()
+    paa.close()
+    tash.close()
