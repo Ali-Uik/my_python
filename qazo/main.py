bb919a03acce721bc877455b0ce30a14576e4208
Ali_Uik <toxic_m@mail.ru>
2021-12-07 12:29:43 +0500

    any changes in Qazo app

--- qazo/main.py
+++ qazo/main.py
@@ -2,23 +2,28 @@ from counter import *
 
 user_name = input('Ismingizni kiriting: ')
 user_age = int(input('Yoshingizni kiriting: '))
+count = []
 with open('users.txt', mode='r', encoding='utf-') as users_file:
     users_data = users_file.read().split(' ')
+
     for item in users_data:
         if item == user_name + str(user_age):
-            file4 = open(f'{user_name.lower()}{str(user_age)}.txt', mode='r', encoding='utf-8')
-            data = file4.read()
-            print(data)
-            file4.close()
+            count.append(item)
 
-n = user_age - 12
-main_info = qazo(n)
-file = open(f'{user_name.lower()}{str(user_age)}.txt', mode='w', encoding='utf-8')
-file.write(main_info)
-file.close()
-file2 = open('users.txt', mode='a', encoding='utf-8')
-file2.write(user_name.lower() + str(user_age) + ' ')
-file2.close()
+if len(count) == 1:
+    file4 = open(f'{user_name.lower()}{str(user_age)}.txt', mode='r', encoding='utf-8')
+    data = file4.read()
+    print(data)
+    file4.close()
+elif len(count) == 0:
+    n = user_age - 12
+    main_info = qazo(n)
+    file = open(f'{user_name.lower()}{str(user_age)}.txt', mode='w', encoding='utf-8')
+    file.write(main_info)
+    file.close()
+    file2 = open('users.txt', mode='a', encoding='utf-8')
+    file2.write(' ' + user_name.lower() + str(user_age) + ' ')
+    file2.close()
 
 # n = user_age - 12
 # main_info = qazo(n)
