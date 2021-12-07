98d54562b0733db42058c3d4d6de7d098fa7b5a6
Ali_Uik <toxic_m@mail.ru>
2021-12-07 11:35:18 +0500

    any changes in Qazo app

--- qazo/main.py
+++ qazo/main.py
@@ -2,20 +2,38 @@ from counter import *
 
 user_name = input('Ismingizni kiriting: ')
 user_age = int(input('Yoshingizni kiriting: '))
+with open('users.txt', mode='r', encoding='utf-') as users_file:
+    users_data = users_file.read().split(' ')
+    for item in users_data:
+        if item == user_name + str(user_age):
+            file4 = open(f'{user_name.lower()}{str(user_age)}.txt', mode='r', encoding='utf-8')
+            data = file4.read()
+            print(data)
+            file4.close()
+
 n = user_age - 12
 main_info = qazo(n)
-file = open(f'{user_name.lower()}{user_age}.txt', mode='w', encoding='utf-8')
+file = open(f'{user_name.lower()}{str(user_age)}.txt', mode='w', encoding='utf-8')
 file.write(main_info)
 file.close()
 file2 = open('users.txt', mode='a', encoding='utf-8')
-file2.write(user_name.lower() + str(user_age)+' ')
+file2.write(user_name.lower() + str(user_age) + ' ')
 file2.close()
-print(f'Assalomu aleykum {user_name.title()}.Sizning qazo namozlaringiz:\n{main_info}')
 
-while True:
-    command = input('Введите команду:')
-    # print(command)
-    if command == 'stop':
-        break
-    elif command == 'help':
-        print('bobdod\npeshin\nasr\nshom\nxufton')
+# n = user_age - 12
+# main_info = qazo(n)
+# file = open(f'{user_name.lower()}{user_age}.txt', mode='w', encoding='utf-8')
+# file.write(main_info)
+# file.close()
+# file2 = open('users.txt', mode='a', encoding='utf-8')
+# file2.write(user_name.lower() + str(user_age) + ' ')
+# file2.close()
+# print(f'Assalomu aleykum {user_name.title()}.Sizning qazo namozlaringiz:\n{main_info}')
+#
+# while True:
+#     command = input('Введите команду:')
+#     # print(command)
+#     if command == 'stop':
+#         break
+#     elif command == 'help':
+#         print('bobdod\npeshin\nasr\nshom\nxufton')
