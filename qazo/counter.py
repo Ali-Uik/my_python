7fa80190c077b61fccbede0b49f040a859a04c03
Ali_Uik <toxic_m@mail.ru>
2021-12-10 16:52:10 +0500

    any changes in counter file

--- qazo/counter.py
+++ qazo/counter.py
@@ -1,33 +1,31 @@
 import json
-
-def qazo(n):
-    day = n * 365
-    bomdod = 2 * day
-    peshin = 4 * day
-    asr = 4 * day
-    shom = 3 * day
-    xufton = 4 * day
-    vitr = 3 * day
-    main_info = {
-        'day': day,
-        'bobdod': bomdod,
-        'peshin': peshin,
-        'ars': asr,
-        'shom': shom,
-        'xufton': xufton,
-        'vitr': vitr
-    }
-    main_json = json.dumps(main_info)
-
-    return main_json
-
-
-# print(qazo(5))
-
-# user_name = input('Ismingizni kiriting: ')
-# user_age = int(input('Yoshingizni kiriting: '))
+from pprint import pprint
 #
+# def qazo(n):
+#     day = n * 365
+#     bomdod = 2 * day
+#     peshin = 4 * day
+#     asr = 4 * day
+#     shom = 3 * day
+#     xufton = 4 * day
+#     vitr = 3 * day
+#     main_info = {
+#         'day': day,
+#         'bobdod': bomdod,
+#         'peshin': peshin,
+#         'ars': asr,
+#         'shom': shom,
+#         'xufton': xufton,
+#         'vitr': vitr
+#     }
+#     main_json = json.dumps(main_info)
 #
+#     return main_json
+
+
+user_name = input('Ismingizni kiriting: ')
+user_age = int(input('Yoshingizni kiriting: '))
+
 # def asr(user_name, user_age):
 #     file = open(f'../qazo/users/{user_name.lower()}{str(user_age)}.txt', mode='r',
 #                 encoding='utf-8')
@@ -39,4 +37,8 @@ def qazo(n):
 #
 # print(asr(user_name, user_age))
 
-# print(asr(user_name, user_age))
+
+file = open(f'../qazo/users/{user_name.lower()}{str(user_age)}.txt', mode='r', encoding='utf-8')
+data = file.read()
+for key, value in data:
+    print(key, value)
