6288117cf5f886eb73c09f62c63e271830cbadbb
Ali-Uik <toxic_m@mail.ru>
2021-12-17 14:26:07 +0500

    hw1 done

--- lesson14/hw1.py
+++ lesson14/hw1.py
@@ -15,8 +15,36 @@ class Computer:
                f'HDD:{self.HDD}\n' \
                f'Монитор:{self.monitor}'
 
+    def owner_info(self):
+        return f'Владелец этого комп.а {self.owner}'
 
-comp1 = Computer('Ali', 'Core i9', 'Lunix', '64Gb', '1Tb', 'LG 27er')
-comp2 = Computer('Bonu', 'Core i11', 'MacOS', '128Gb', '2Tb', 'Immer')
+    def __eq__(self, other):
+        return self.RAM == other.RAM
+
+    def __ne__(self, other):
+        return self.RAM != other.RAM
+
+    def __gt__(self, other):
+        return self.RAM > other.RAM
+
+    def __ge__(self, other):
+        return self.RAM >= other.RAM
+
+    def __lt__(self, other):
+        return self.RAM < other.RAM
+
+    def __le__(self, other):
+        return self.RAM <= other.RAM
+
+
+comp1 = Computer('Ali', 'Core i9', 'Lunix', '64', '1Tb', 'LG 27er')
+comp2 = Computer('Bonu', 'Core i11', 'MacOS', '128', '2Tb', 'Immer')
 print(comp1)
 print(comp2)
+# print(comp2)
+print(comp1 > comp2)
+print(comp1 >= comp2)
+print(comp1 == comp2)
+print(comp1 != comp2)
+print(comp1 < comp2)
+print(comp1 <= comp2)
