c39b098b0d0409b3451a9b8768c8c98b36464529
Ali-Uik <toxic_m@mail.ru>
2022-01-13 16:26:27 +0500

    'lesson21_bot'

--- lesson20/main.py
+++ lesson20/main.py
@@ -2,6 +2,7 @@ import requests
 import psycopg2
 from pprint import pprint
 
+# Маълумотлар базасига уланиш
 db = psycopg2.connect(
     database='lesson19',
     host='localhost',
@@ -23,6 +24,7 @@ FROM products ORDER BY unit_price;
 )
 
 products = cursor.fetchall()  # Для получения ответов с более чем 1 данных
+# products = cursor.fetchone()  # Для получения 1 данных
 pprint(products)
 
 cursor.execute(
@@ -40,8 +42,43 @@ print(avg1)
 
 cursor.execute(
     '''
+    DROP TABLE IF EXISTS posts;
     DROP TABLE IF EXISTS users;
-    CREATE TABLE IF NOT EXISTS users
-        id
+    CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name VARCHAR(100),
+        username VARCHAR(100),
+        email TEXT
+        );
+    CREATE TABLE IF NOT EXISTS posts(
+        post_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        user_id INTEGER REFERENCES users(user_id),
+        title TEXT,
+        body TEXT
+        );
     '''
 )
+
+users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+posts = requests.get('https://jsonplaceholder.typicode.com/posts').json()
+for user in users:
+    name = user['name']
+    username = user['username']
+    email = user['email']
+    cursor.execute(
+        '''
+        INSERT INTO users(name,username,email) VALUES (%s,%s,%s)
+        ''', (name, username, email)
+    )
+
+for post in posts:
+    title = post['title']
+    body = post['body']
+    user_id = post['userId']
+    cursor.execute(
+        '''
+        INSERT INTO posts(title,body,user_id) VALUES (%s,%s,%s)
+        ''', (title, body, user_id)
+    )
+db.commit()
+db.close()
--- tub.py
+++ tub.py
@@ -1,10 +1,27 @@
-A = int(input('A dan N gacha tub sonlarni topamiz A='))
-N = int(input('N='))
-for k in range(A, N + 1):
-    prime = True
-    for i in range(2, k):
-        if k % i == 0:
-            prime = False
-            break
-    if prime:
-        print('{} - tub sonlar'.format(k))
+# A = int(input('A dan N gacha tub sonlarni topamiz A='))
+# N = int(input('N='))
+# for k in range(A, N + 1):
+#     prime = True
+#     for i in range(2, k):
+#         if k % i == 0:
+#             prime = False
+#             break
+#     if prime:
+#         print('{} - tub sonlar'.format(k))
+#
+print('ABC uchburchak perimetri p ni va yuzi S ni topamiz.')
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    print(f'ABC uchburchakning perimetri p={a + b + c}')
+    print(
+        f'ABC uchburchakning yuzi S = {((a + b + c) / 2 * (a + b - c) / 2 * (a + c - b) / 2 * (b + c - a) / 2) ** (1 / 2)}')
+    if (a ** 2 + b ** 2 > c ** 2) or (a ** 2 + c ** 2 > b ** 2) or (b ** 2 + c ** 2 > a ** 2):
+        print('ABC uchburchak o\'tkir burchakli uchburchak')
+    elif (a ** 2 + b ** 2 == c ** 2) or (a ** 2 + c ** 2 == b ** 2) or (b ** 2 + c ** 2 == a ** 2):
+        print('ABC uchburchak to\'g\'ri burchakli uchburchak')
+    else:
+        print('ABC uchburchak o\'tmas burchakli uchburchak')
+else:
+    print('bunday uchburchak mavjud emas')
