b9a83d9622a9e72b66a0078b237b1fac68db7b20
Ali_Uik <toxic_m@mail.ru>
2022-02-11 16:32:49 +0500

    'weatherBot_done'

--- weatherBot/main.py
+++ weatherBot/main.py
@@ -1,13 +1,11 @@
 from aiogram import Bot, Dispatcher, executor, types
-from countryinfo import CountryInfo
+import requests
+
 # Bot - объект бота, Dispatcher - слушатель действий бота, executor - запускатель бота
 from aiogram.types import Message, CallbackQuery, Location, ReplyKeyboardMarkup
 # Типы данных, которые приходят боту
 import os
 from dotenv import load_dotenv
-
-# import sqlite3
-# from keyboards import *
 
 load_dotenv()
 token = os.getenv('TOKEN')
@@ -17,13 +15,42 @@ dp = Dispatcher(bot)  # Создали объект слежки ботам
 
 @dp.message_handler(content_types=['location'])
 async def handle_location(message: types.Message):
-    print(type(message))
-    lat = message.location.latitude
-    lon = message.location.longitude
-    print(message)
-    print(lat, lon)
-    reply = "latitude:  {}\nlongitude: {}".format(lat, lon)
+    current_lat = message.location.latitude
+    current_lon = message.location.longitude
+    location_name1 = location_name(current_lat, current_lon)
+    print(location_name1)
+    data = weather(location_name1)
+    temp = data['main']['temp']
+    humidity = data['main']['humidity']
+    pressure = data['main']['pressure']
+    wind = data['wind']['speed']
+    country = data['sys']['country']
+    reply = "Cтрана: {}\nМесто: {}\nТемпература:  {} °C\nВетер: {} км/ч\nВлажность: {} %\nДавление: {} мм рт. ст." \
+        .format(country, location_name1, temp, wind, humidity, pressure)
     await message.answer(reply)
+
+
+def location_name(current_lat, current_lon):
+    params = {
+        'lat': current_lat,
+        'lon': current_lon,
+        'limit': 4,
+        'appid': 'd0c4c1e4babd454e692db213ba832310',  # myAPI keys in openweathermap.org
+    }
+    data = requests.get('https://api.openweathermap.org/data/2.5/weather?', params=params).json()
+    location_name = data['name']
+    return location_name
+
+
+def weather(location_name):
+    params = {
+        'appid': 'd0c4c1e4babd454e692db213ba832310',  # myAPI keys in openweathermap.org
+        'units': 'metric',
+        'lang': 'ru'
+    }
+    params['q'] = location_name
+    data = requests.get('https://api.openweathermap.org/data/2.5/weather?', params=params).json()
+    return data
 
 
 executor.start_polling(dp, skip_updates=True)
